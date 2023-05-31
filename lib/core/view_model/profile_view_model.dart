e7f4067f048afc3e638e362c51b92e17c1f2b3ff
Abegy <ighat17@gmail.com>
2023-05-31 19:26:50 +0100

    Profile page and logout function updated

--- lib/core/view_model/profile_view_model.dart
+++ lib/core/view_model/profile_view_model.dart
@@ -1,17 +1,42 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pfa_v2/helper/local_storage_data.dart';
+import 'package:pfa_v2/model/user_model.dart';
 
 class ProfileViewModel extends GetxController{
-
+    ValueNotifier<bool> get loading => _loading;
+    ValueNotifier<bool> _loading = new ValueNotifier(false);
     final LocalStorageData localStorageData = Get.find();
 
+    UserModel get userModel => _userModel;
+    UserModel _userModel;
+
+    @override
+    void onInit() {
+      // TODO: implement onInit
+      super.onInit();
+      getCurrentUser();
+    }
+
+    void getCurrentUser() async{
+      _loading.value = true;
+       await localStorageData.getUser.then((value){
+          _userModel = value;
+       });
+       _loading.value = false;
+       update();
+    }
     Future<void> signOut()async{
     GoogleSignIn().signOut();
     FirebaseAuth.instance.signOut();
     localStorageData.deleteUser();
   }
+
+
 
 }
--- lib/view/profile_view.dart
+++ lib/view/profile_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pfa_v2/core/view_model/profile_view_model.dart';
 import 'package:pfa_v2/view/auth/login_view.dart';
+import 'package:pfa_v2/view/widgets/custom_text.dart';
 
 class ProfileView extends StatelessWidget {
 
@@ -9,17 +10,199 @@ class ProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<ProfileViewModel>(
       init: ProfileViewModel(),
-      builder:(controller)=> Scaffold(
+      builder:(controller)=> controller.loading.value
+        ? Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
         body: Container(
-          child: Center(
-            child: TextButton(
-              onPressed:(){
-                controller.signOut();
-                Get.offAll(LoginView());
-              },
-              child: Text("SignOut"),
+          padding: EdgeInsets.only(top: 50),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(100),
+                          ),
+                          image: DecorationImage(
+                                  image: controller.userModel == null
+                                      ? AssetImage(
+                                          'assets/images/avatar.png',
+                                        )
+                                      : controller.userModel.pic == 'default'
+                                ? AssetImage('assets/images/avatar.png')
+                                : NetworkImage(
+                                  controller.userModel.pic,
+                                ),
+                          fit: BoxFit.fill,
+                          ),
+                      ),
+                      ),
+                      Column(
+                        children: [
+                          CustomText(
+                            text: controller.userModel.name,
+                            color: Colors.black,
+                            fontSize: 32,
+                          ),
+                          CustomText(
+                            text: controller.userModel.email,
+                            color: Colors.black,
+                            fontSize: 24,
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ),
+
+                SizedBox(height: 100,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: (){
+
+                    },
+                    child: ListTile(
+                      title: CustomText(
+                        text: 'Edit Profile',
+                      ),
+
+                      leading: Image.asset(
+                          'assets/menu_icon/edit_profile.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: (){
+
+                    },
+                    child: ListTile(
+                      title: CustomText(
+                        text: 'Shipping Address',
+                      ),
+
+                      leading: Image.asset(
+                        'assets/menu_icon/shipping_address.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: (){
+
+                    },
+                    child: ListTile(
+                      title: CustomText(
+                        text: 'Order History',
+                      ),
+
+                      leading: Image.asset(
+                        'assets/menu_icon/order_history.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: (){
+
+                    },
+                    child: ListTile(
+                      title: CustomText(
+                        text: 'Cards',
+                      ),
+
+                      leading: Image.asset(
+                        'assets/menu_icon/credit-cards.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: (){
+
+                    },
+                    child: ListTile(
+                      title: CustomText(
+                        text: 'Notifications',
+                      ),
+
+                      leading: Image.asset(
+                        'assets/menu_icon/notification.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,
+                ),
+                Container(
+                  child: TextButton(
+                    onPressed: (){
+                      controller.signOut();
+                      Get.offAll(LoginView());
+                    },
+                    child: ListTile(
+                      title: CustomText(
+                        text: 'Log Out',
+                      ),
+
+                      leading: Image.asset(
+                        'assets/menu_icon/logout.png',
+                      ),
+                      trailing: Icon(
+                        Icons.navigate_next,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,
+                ),
+              ],
             ),
-          ),
+          )
         ),
       ),
     );
